7863a5f33d682c9dc9871ca0db7f89e5666ec4a0
Yuito Fujiwara <yuito6187@gmail.com>
2025-09-15 17:44:47 -0700

    validate email addresses with regular expressions

--- mailer.js
+++ mailer.js
@@ -10,8 +10,13 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 exports.sendEmail = async (data) => {
   try {
+    if (!emailRegex.test(data.email)) {
+      throw new Error("Invalid email address");
+    }
     const htmlMail = `<p>Thank you for sending a message. The submitted form is below.</p><p>from ${data.firstname} ${data.lastname} (${data.email})</p><p>${data.message}</p>`
     const textMail = `Thank you for sending a message. The submitted form is below.\nfrom ${data.firstname} ${data.lastname} (${data.email})\n${data.message}`
     const info = await transporter.sendMail({
@@ -23,6 +28,7 @@ exports.sendEmail = async (data) => {
     });
     return info;
   } catch (error) {
+    console.error(error)
     throw new Error("Error occurred while sending the email.");
   }
 }
